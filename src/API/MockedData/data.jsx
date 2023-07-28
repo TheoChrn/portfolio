93252d83456be7e4b9b83aa7396ab0949059456c
Theo Cheron <theo.chrn@gmail.com>
2023-07-28 22:49:49 +0200

    Update LinkedIn

--- src/API/MockedData/data.jsx
+++ src/API/MockedData/data.jsx
@@ -43,7 +43,7 @@ export const logos = [
       },
       {
         logo: <LinkedIn mainColor="white" size="30px" />,
-        link: "https://www.linkedin.com/",
+        link: "https://www.linkedin.com/in/th%C3%A9o-c-7232b6180/",
       },
       {
         logo: <Email mainColor="white" size="30px" />,
